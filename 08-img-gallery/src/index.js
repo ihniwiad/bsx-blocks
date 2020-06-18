fb5a4729e6556100a9992942b9cb1d09ab635e32
ihniwiad <mail@matthiasbroecker.de>
2020-06-19 01:55:35 +0200

    UPDATE: todo: replace icon buttons

--- 08-img-gallery/src/index.js
+++ 08-img-gallery/src/index.js
@@ -13,6 +13,24 @@ const {
 } = wp.components;
 
 
+/*
+    TODO: add icons to button (non icon button)
+
+    - arrow left:
+        <svg aria-hidden="true" role="img" focusable="false" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" class="dashicon dashicons-arrow-left-alt2"><path d="M14 5l-5 5 5 5-1 2-7-7 7-7z"></path></svg>
+
+    - arrow right: 
+        <svg aria-hidden="true" role="img" focusable="false" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" class="dashicon dashicons-arrow-right-alt2"><path d="M6 15l5-5-5-5 1-2 7 7-7 7z"></path></svg>
+
+    - trash bin:
+        <svg aria-hidden="true" role="img" focusable="false" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" class="dashicon dashicons-trash"><path d="M12 4h3c.6 0 1 .4 1 1v1H3V5c0-.6.5-1 1-1h3c.2-1.1 1.3-2 2.5-2s2.3.9 2.5 2zM8 4h3c-.2-.6-.9-1-1.5-1S8.2 3.4 8 4zM4 7h11l-.9 10.1c0 .5-.5.9-1 .9H5.9c-.5 0-.9-.4-1-.9L4 7z"></path></svg>
+
+    - circle plus:
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 24 24" width="24" height="24" role="img" aria-hidden="true" focusable="false"><path d="M10 1c-5 0-9 4-9 9s4 9 9 9 9-4 9-9-4-9-9-9zm0 16c-3.9 0-7-3.1-7-7s3.1-7 7-7 7 3.1 7 7-3.1 7-7 7zm1-11H9v3H6v2h3v3h2v-3h3V9h-3V6zM10 1c-5 0-9 4-9 9s4 9 9 9 9-4 9-9-4-9-9-9zm0 16c-3.9 0-7-3.1-7-7s3.1-7 7-7 7 3.1 7 7-3.1 7-7 7zm1-11H9v3H6v2h3v3h2v-3h3V9h-3V6z"></path></svg>
+
+*/
+
+
 registerBlockType( 'bsx-blocks/img-gallery', {
     title: __( 'BSX Image Gallery', 'bsx-blocks' ),
     icon: 'format-gallery',
