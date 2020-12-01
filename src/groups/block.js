503692d29d93b739c3d4517e0879bce7747bd907
ihniwiad <mail@matthiasbroecker.de>
2020-12-01 12:43:03 +0100

    UPDATE: added template UPDATE: use external svg file

--- src/groups/block.js
+++ src/groups/block.js
@@ -24,13 +24,12 @@ const {
 } = wp.data;
 
 
+import { svgIcon } from './../_functions/wp-icons.js';
+
+
 registerBlockType( 'bsx-blocks/groups', {
     title: __( 'BSX Groups', 'bsx-blocks' ),
-    icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" aria-hidden="true" role="img" focusable="false">
-            <path d="M7,7h6c0.55,0,1-0.45,1-1V4c0-0.55-0.45-1-1-1H7C6.45,3,6,3.45,6,4v2C6,6.55,6.45,7,7,7z M13,13H7c-0.55,0-1,0.45-1,1v2 c0,0.55,0.45,1,1,1h6c0.55,0,1-0.45,1-1v-2C14,13.45,13.55,13,13,13z M9.5,11V9c0-0.55-0.45-1-1-1H3C2.45,8,2,8.45,2,9v2 c0,0.55,0.45,1,1,1h5.5C9.05,12,9.5,11.55,9.5,11z M17,8h-5.5c-0.55,0-1,0.45-1,1v2c0,0.55,0.45,1,1,1H17c0.55,0,1-0.45,1-1V9 C18,8.45,17.55,8,17,8z"/>
-        </svg>
-    ),
+    icon: svgIcon( 'groups' ),
     category: 'layout',
     attributes: {
         templateName: {
@@ -69,11 +68,7 @@ registerBlockType( 'bsx-blocks/groups', {
             {
                 name: 'lazy-img-paragraph-button',
                 title: __( 'Lazy image, paragraph, button', 'bsx-blocks' ),
-                icon: (
-                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" role="img" aria-hidden="true" focusable="false">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M39,12H9c-1.1,0-2,0.9-2,2v20c0,1.1,0.9,2,2,2h30c1.1,0,2-0.9,2-2V14C41,12.9,40.1,12,39,12z M39,34H9V14h30V34z M22.49,31.03h2.35v-2.21h-2.35V31.03z M22.51,19.38c0.36-0.28,0.81-0.42,1.35-0.42c0.63,0,1.1,0.17,1.43,0.5s0.49,0.81,0.49,1.44 c0,0.54-0.14,1.03-0.43,1.45s-0.68,0.89-1.18,1.38c-0.71,0.6-1.16,1.09-1.35,1.46s-0.3,1.02-0.3,1.94h2.3 c0-0.56,0.04-0.97,0.13-1.25s0.32-0.58,0.68-0.9c0.72-0.58,1.31-1.21,1.77-1.88c0.46-0.67,0.69-1.42,0.69-2.23 c0-1.22-0.37-2.16-1.09-2.83c-0.73-0.67-1.77-1-3.12-1c-1.25,0-2.26,0.31-3.02,0.94c-0.76,0.62-1.15,1.52-1.17,2.69h2.3 C21.97,20.09,22.15,19.66,22.51,19.38z"/>
-                    </svg>
-                ),
+                icon: svgIcon( 'groups-lazy-img-paragraph-button' ),
                 attributes: {},
                 template: [ 
                     [ 
@@ -89,6 +84,31 @@ registerBlockType( 'bsx-blocks/groups', {
                     [
                         'bsx-blocks/buttons',
                         {},
+                    ],
+                ],
+                templateLock: false,
+            },
+            {
+                name: 'lazy-img-heading-paragraph',
+                title: __( 'Lazy image, heading, paragraph', 'bsx-blocks' ),
+                icon: svgIcon( 'groups-lazy-img-heading-paragraph' ),
+                attributes: {},
+                template: [ 
+                    [ 
+                        'bsx-blocks/lazy-img', 
+                        {},
+                    ], 
+                    [
+                        'core/heading',
+                        { 
+                            placeholder: 'Add heading, configure heading level...', 
+                        },
+                    ],
+                    [
+                        'core/paragraph',
+                        { 
+                            placeholder: 'Add text...', 
+                        },
                     ],
                 ],
                 templateLock: false,
